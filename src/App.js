76cf6e5779f66d4eeb13480f3309cd90db665856
Tharunkumar <tharunkumarnani0116@gmail.com>
2023-11-02 16:54:24 +0000

    initial

---
+++ src/App.js
@@ -0,0 +1,329 @@
+import {Component} from 'react'
+
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="home-container">
+        <h1 className="heading">
+          &quot;Some people call this artificial intelligence, but the reality
+          is this technology will enhance us. So, instead of artificial
+          intelligence, I think we’ll augment our intelligence” - Ginni Rometty
+          – Former CEO & President, IBM&quot;
+        </h1>
+        <div className="card1">
+          <div className="img-card1">
+            <img
+              alt="checkout"
+              className="standing-img"
+              src="https://resources.commlabindia.com/hs-fs/hubfs/webinars/corporate-training-with-ai-banner-image.png?width=700&height=978&name=corporate-training-with-ai-banner-image.png"
+            />
+          </div>
+          <div className="card1-info">
+            <p className="card1-des">The Top Trending Topic of the Year</p>
+            <h1>Unleashing the Potential of AI in Corporate Training</h1>
+            <ul className="check-list-orange">
+              <li>
+                <img
+                  alt="tick mark"
+                  src="https://resources.commlabindia.com/hubfs/cli-main-site/pillar-pages/checkmark-orange.svg"
+                  className="orange-tick"
+                />
+                <p className="tick-des">
+                  Learn how AI is helping corporates upskill and reskill
+                  workforce
+                </p>
+              </li>
+              <li>
+                <img
+                  alt="tick mark"
+                  src="https://resources.commlabindia.com/hubfs/cli-main-site/pillar-pages/checkmark-orange.svg"
+                  className="orange-tick"
+                />
+                <p className="tick-des">
+                  Walk away with tips, tools, and resources to get started with{' '}
+                  AI-empowered training
+                </p>
+              </li>
+            </ul>
+            <h2>JOIN DR RK Prasad, Shalini, Rajesh LIVE</h2>
+            <div className="time-date">
+              <div>
+                <strong>Tuesday, January 09, 2024</strong>
+              </div>
+              <div>
+                <strong>11 AM Eastern | 8 AM Pacific</strong>
+              </div>
+              <div>
+                <strong>Duration: 90 Minutes</strong>
+              </div>
+              <div>
+                <strong>
+                  <span className="price">$997 VALUE</span>
+                  <span>&nbsp;</span>
+                </strong>
+                <span className="span-style">
+                  <i>FREE!</i>
+                </span>
+              </div>
+              <button type="button" className="seat-btn">
+                SECURE YOUR SEAT
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="companies">
+          <img
+            src="https://resources.commlabindia.com/hubfs/webinars/logos-v2.png"
+            className="company-logo"
+            alt="Companies logo"
+          />
+        </div>
+        <div className="you-learn">
+          <h1>YOU WILL LEARN</h1>
+          <ul className="check-list-orange">
+            <li>
+              <img
+                alt="tick mark"
+                src="https://resources.commlabindia.com/hubfs/cli-main-site/pillar-pages/checkmark-orange.svg"
+                className="orange-tick"
+              />
+              <strong className="tick-des2">
+                Evolution of AI and Corporate Training
+              </strong>
+            </li>
+            <li>
+              <img
+                alt="tick mark"
+                src="https://resources.commlabindia.com/hubfs/cli-main-site/pillar-pages/checkmark-orange.svg"
+                className="orange-tick"
+              />
+              <strong className="tick-des2">
+                How AI can close skills gaps with rapid upskilling and
+                reskilling
+              </strong>
+            </li>
+            <li>
+              <img
+                alt="tick mark"
+                src="https://resources.commlabindia.com/hubfs/cli-main-site/pillar-pages/checkmark-orange.svg"
+                className="orange-tick"
+              />
+              <strong className="tick-des2">
+                Steps to integrate AI tools in training programs
+              </strong>
+            </li>
+            <li>
+              <img
+                alt="tick mark"
+                src="https://resources.commlabindia.com/hubfs/cli-main-site/pillar-pages/checkmark-orange.svg"
+                className="orange-tick"
+              />
+              <strong className="tick-des2">
+                Challenges and opportunities with AI implementation
+              </strong>
+            </li>
+            <li>
+              <img
+                alt="tick mark"
+                src="https://resources.commlabindia.com/hubfs/cli-main-site/pillar-pages/checkmark-orange.svg"
+                className="orange-tick"
+              />
+              <strong className="tick-des2">
+                Roadmap to align training with business goals{' '}
+              </strong>
+              <strong> </strong>
+            </li>
+            <li>
+              <img
+                alt="tick mark"
+                src="https://resources.commlabindia.com/hubfs/cli-main-site/pillar-pages/checkmark-orange.svg"
+                className="orange-tick"
+              />
+              <strong className="tick-des2">
+                Live action – creating assets with Vyond Go, Synthesia, Quizbot
+              </strong>
+              <strong> </strong>
+            </li>
+            <li>
+              <img
+                alt="tick mark"
+                src="https://resources.commlabindia.com/hubfs/cli-main-site/pillar-pages/checkmark-orange.svg"
+                className="orange-tick"
+              />
+              <strong className="tick-des2">
+                Best way Learning Architects can use AI{' '}
+              </strong>
+            </li>
+          </ul>
+          <h3>
+            <strong>
+              Unleashing the Potential of
+              <br />
+              AI in Corporate Training
+            </strong>
+          </h3>
+          <h6>
+            <strong>
+              The Impact of AI on L&amp;D – Insights and Applications
+            </strong>
+          </h6>
+          <h4>
+            <strong>
+              Tuesday, January 09, 2024
+              <br />
+              11 am Eastern | 8 am Pacific
+              <br />
+              Duration: 90 Minutes
+            </strong>
+          </h4>
+          <button className="seat-btn2" type="button">
+            SECURE YOUR SEAT
+            <br />
+            <span className="strike">$997 VALUE</span>
+            <span> - FREE!</span>
+          </button>
+        </div>
+        <div className="hosts-cont">
+          <h1>MEET YOUR HOSTS</h1>
+          <div className="host-items-cont">
+            <div className="host-item">
+              <img
+                src="https://resources.commlabindia.com/hubfs/webinars/rk-prasad-ai-webinar.jpg"
+                className="host-img"
+                alt="host"
+              />
+              <p className="host-name">RK Prasad</p>
+              <p className="host-rule">CEO & Co Founder</p>
+              <p className="host-des">
+                RK has nurtured CommLab India from concept to commercial
+                success, and is responsible for formulating the business
+                strategy. He is also responsible for nurturing customer
+                relationships.{' '}
+              </p>
+              <p className="host-des">
+                An entrepreneur at heart, RK has 35 years of experience in
+                Sales, Corporate training, University teaching, and eLearning.
+                He regularly conducts seminars and webinars for customers across
+                the world on various topics of technology-enhanced learning.
+              </p>
+              <p className="host-des">
+                RK holds a PhD in Mobile Learning from Lancaster University, UK,
+                and an MBA.
+              </p>
+              <p className="host-des">
+                A good teacher and engaging trainer, RK helps people learn and
+                bloom. His priorities are his employees, his customers, and his
+                community.
+              </p>
+            </div>
+            <div className="host-item">
+              <img
+                src="https://resources.commlabindia.com/hubfs/cli-main-site/about-us/Shalini.jpg"
+                className="host-img"
+                alt="host"
+              />
+              <p className="host-name">Shalini Merugu</p>
+              <p className="host-rule">Director - Learning Solutions</p>
+              <p className="host-des">
+                Shalini is a versatile learning design professional with 16+
+                years of experience in instructional design (ILT, VILT, and
+                eLearning).
+              </p>
+              <p className="host-des">
+                Her experience in designing eLearning solutions along with
+                designing and delivering instructor-led training helps her
+                leverage the strengths of each medium to consistently create
+                impactful training solutions, ensuring that nothing is lost when
+                converting from one format to another.
+              </p>
+              <p className="host-des">
+                Apart from improving the quality of learning solutions, she also
+                conducts customer education workshops at customer sites globally
+                and presents webinars on learning design.
+              </p>
+            </div>
+            <div className="host-item">
+              <img
+                src="https://resources.commlabindia.com/hubfs/cli-main-site/about-us/rajesh-v4.jpg"
+                className="host-img"
+                alt="host"
+              />
+              <p className="host-name">Rajesh Venkata</p>
+              <p className="host-rule">Director - Innovations</p>
+              <p className="host-des">
+                Rajesh started his career as a Quality Executive in 2008, and
+                gained expertise in eLearning, digital content delivery, and
+                project management, over these 15+ years.
+              </p>
+              <p className="host-des">
+                Rajesh has an excellent track record of team management and
+                operational planning, along with good knowledge of eLearning
+                authoring tools such as Storyline, Captivate, and Lectora.
+              </p>
+              <p className="host-des">
+                Well-experienced in managing multiple projects for global
+                clients, Rajesh has a keen eye for trends in the tech-enabled
+                learning space. This made him the natural choice for the current
+                role, where he researches the latest tools and tech, and guides
+                the CommLab India team in leveraging them to address training
+                needs faster, better, and er.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="last-book-banner">
+          <h3>
+            <strong>
+              Unleashing the Potential of AI in Corporate Training
+            </strong>
+          </h3>
+          <h6>
+            <strong>
+              The Impact of AI on L&amp;D – Insights and Applications
+            </strong>
+          </h6>
+          <h4>
+            <strong>
+              Tuesday, January 09, 2024
+              <br />
+              11 am Eastern | 8 am Pacific
+              <br />
+              Duration: 90 Minutes
+            </strong>
+          </h4>
+          <button className="seat-btn2" type="button">
+            SECURE YOUR SEAT
+            <br />
+            <span className="strike">$997 VALUE</span>
+            <span> - FREE!</span>
+          </button>
+        </div>
+        <div className="bottom-sec">
+          <div className="left-sec">
+            <img
+              src="https://resources.commlabindia.com/hubfs/commlab_logo/cl-logo-final-white.png"
+              className="company-bottom"
+              alt="logo"
+            />
+            <p className="bot-des">Copyright © 2023, CommLab India</p>
+          </div>
+          <div className="links-cont">
+            <a
+              className="links"
+              href="https://www.commlabindia.com/privacy-policy"
+            >
+              Privacy Policy |{' '}
+            </a>
+            <a className="links" href="https://www.commlabindia.com/sitemap">
+              Site Map
+            </a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
